7c84787cfc1de94fc957583e7be44e0378103943
Krister Viirsaar <krister.viirsaar@gmail.com>
2017-08-10 18:27:33 +0300

    First dash command

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const child_process = require('child_process')
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+	const govdata = await new Promise((resolve, reject) => {
+		child_process.exec('dash-cli getgovernanceinfo', (err, result) => {
+			if (err)
+				reject(err)
+			else
+				resolve(result)
+		})
+	}).catch((e) => {
+		res.send(e)
+	})
+	res.send(JSON.stringify(govdata))
 })
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!')
 })
