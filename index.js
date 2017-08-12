fc1fb69025fc12a068ed1f5466d4e82e85f21a07
Krister Viirsaar <krister.viirsaar@gmail.com>
2017-08-12 23:01:00 +0300

    make API human readable in browser

--- index.js
+++ index.js
@@ -12,15 +12,29 @@ app.get('/', async (req, res) => {
 					resolve(result)
 			})
 		})
-		res.send(JSON.stringify(govdata))
+		res.send(dataToResponseJSON(govdata, req))
 	} catch(e) {
 		let humanReadable = {
-			cmd: e.cmd,
-			message: e.toString()
+			command: e.cmd,
+			message: e.toString().replace(/\n/g, '; ')
 		};
-		res.send(JSON.stringify(humanReadable, null, 4))
+		res.send(dataToResponseJSON(humanReadable, req))
 	}
 })
+
+function dataToResponseJSON(data, req) {
+	// Don't trust strings, formatting may look different
+	if (typeof data === 'string')
+		data = JSON.parse(data)
+
+	let formatted = JSON.stringify(data, null, 4)
+
+	// Make response readable in browser
+	if (req.headers['user-agent'])
+		formatted = '<pre>' + formatted + '</pre>'
+
+	return formatted
+}
 
 app.listen(3000, () => {
 	console.log('Example app listening on port 3000!')
