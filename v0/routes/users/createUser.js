7057f6d1028fc214b16234b5ff2f1ca9d80991c6
Jordan Schroter <jordan.schroter@gmail.com>
2017-09-04 14:22:52 -0400

    be explicity about body attributes
    avoids attempting to saving unnecessary properties causing potential db errors

--- v0/routes/users/createUser.js
+++ v0/routes/users/createUser.js
@@ -3,26 +3,27 @@ const { BadRequestError } = require('../../errors')
 const validateAttributes = require('./validateAttributes')
 
 module.exports = async (req, res) => {
-	const attrs = req.body
+	const { username, email, password } = req.body
 
-	const errors = await validateAttributes(attrs)
+	const errors = await validateAttributes({ username, email, password })
 	if (errors) throw new BadRequestError(errors[0])
 
-	const existingUsername = await User.findOne({ username: attrs.username })
+	const existingUsername = await User.findOne({ username: username })
 	if (existingUsername) throw new BadRequestError('username is taken')
 
-	if (attrs.email) {
-		const existingEmail = await User.findOne({ email: attrs.email })
+	if (email) {
+		const existingEmail = await User.findOne({ email: email })
 		if (existingEmail) throw new BadRequestError('email is taken')
 		// TODO: email confirmation
 	}
 
-	const user = await User.create(
-		Object.assign({}, attrs, {
-			emailConfirmationHash: null,
-			emailConfirmed: false
-		})
-	)
+	const user = await User.create({
+		username,
+		email,
+		password,
+		emailConfirmationHash: null,
+		emailConfirmed: false
+	})
 
 	res.json(User.stripPrivateFields(user))
 }
