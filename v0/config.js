c70421253d3a67000ef36f87c8d4bd67ba41a812
Krister Viirsaar <krister.viirsaar@gmail.com>
2017-09-27 18:44:20 +0300

    Separate containers: heliumd and backend. (#19)
    * heliumd compiler
    
    * containers separated
    
    * simplify networks

--- v0/config.js
+++ v0/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'cli-command': 'dash-cli',
+	'cli-command': 'helium-cli',
 	lockedVotingBlocks: 1662 // https://dashpay.atlassian.net/wiki/spaces/DOC/pages/19169430/Using+Decentralized+Governance+Proposals+Voting+and+Budgets
 }
