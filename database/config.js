b683190edbcbd18d229dac594cceb6db239c67f9
Jordan Schroter <jordan.schroter@gmail.com>
2017-10-07 05:41:44 -0400

    add ssl config for production db (#23)

--- database/config.js
+++ database/config.js
@@ -5,7 +5,8 @@ module.exports = {
 		port: process.env.DB_PORT,
 		database: process.env.DB_NAME,
 		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD
+		password: process.env.DB_PASSWORD,
+		ssl: !!process.env.DB_SSL
 	},
 	migrations: {
 		tableName: 'migrations',
